2df0d45188620fe9d7250496c321d067ee97eeb7
Jordan Abe <j.abe@costamar.com>
2018-05-03 14:41:46 -0500

    Se agrega url para consumir los servicios getPeriodos, pagar deudas, confirmar pago de deudas

--- app/src/main/java/pe/edu/upc/deudasapp/network/NewsApi.java
+++ app/src/main/java/pe/edu/upc/deudasapp/network/NewsApi.java
@@ -6,14 +6,18 @@ package pe.edu.upc.deudasapp.network;
 
 public class NewsApi {
 
-    private static String BASE_URL = "http://192.168.1.37:8081/deudas-service2";
+    private static String BASE_URL = "http://192.168.50.45:8081/deudas-service2";
 
     public static String getDeudasUrl() {
         return BASE_URL + "/deudas";
     }
 
-    public static String getDeudaUrl() {
-        return BASE_URL + "/deuda";
-    }
+    public static String getDeudaUrl() {return BASE_URL + "/deuda";}
+
+    public static String getPagarUrl() {return BASE_URL + "/pagarDeuda";}
+
+    public static String getConfirmarUrl() {return BASE_URL + "/confirmarDeuda";}
+
+    public static String getPeriodosUrl() {return BASE_URL + "/periodos";}
 
 }
