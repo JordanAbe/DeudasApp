1700ba9c27fde5c271b2fd91cdbb3287720db33d
Jordan Abe <j.abe@costamar.com>
2018-05-03 14:44:58 -0500

    Se agregan constantes para par el tipo de usuario y estados

---
+++ app/src/main/java/pe/edu/upc/deudasapp/util/Constantes.java
@@ -0,0 +1,16 @@
+package pe.edu.upc.deudasapp.util;
+
+/**
+ * Created by User on 01/05/2018.
+ */
+
+public class Constantes {
+
+    public static int INQUILINO = 1;
+    public static int ADMINISTRADOR = 0;
+
+    public static String ESTADO_PENDIENTE = "Pendiente";
+    public static String ESTADO_EN_PROCESO = "En proceso";
+    public static String ESTADO_PAGADO = "Pagado";
+
+}
