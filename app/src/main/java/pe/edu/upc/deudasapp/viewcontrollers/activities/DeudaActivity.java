e03c189a5d758cb20bb11c21f0eba54baba89d13
MarkanDeveloper <markant.developer@gmail.com>
2018-05-04 19:48:38 -0500

    se implementa código para validar visibilidad de botones según estado de la deuda, se implementa botón Pagar deuda, se implementa botón Listado para redireccionar al listado de deudas

--- app/src/main/java/pe/edu/upc/deudasapp/viewcontrollers/activities/DeudaActivity.java
+++ app/src/main/java/pe/edu/upc/deudasapp/viewcontrollers/activities/DeudaActivity.java
@@ -1,25 +1,27 @@
 package pe.edu.upc.deudasapp.viewcontrollers.activities;
 
+
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Configuration;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
-import android.widget.ImageButton;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.androidnetworking.AndroidNetworking;
 import com.androidnetworking.common.Priority;
 import com.androidnetworking.error.ANError;
 import com.androidnetworking.interfaces.JSONObjectRequestListener;
-import com.androidnetworking.widget.ANImageView;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -30,24 +32,37 @@ import java.util.List;
 import pe.edu.upc.deudasapp.R;
 import pe.edu.upc.deudasapp.models.Deuda;
 import pe.edu.upc.deudasapp.models.DeudaDetalle;
+import pe.edu.upc.deudasapp.models.Usuario;
 import pe.edu.upc.deudasapp.network.NewsApi;
+import pe.edu.upc.deudasapp.util.Constantes;
 import pe.edu.upc.deudasapp.viewcontrollers.adapters.DeudaDetalleAdapter;
 import pe.edu.upc.deudasapp.viewcontrollers.adapters.DeudasAdapter;
+import pe.edu.upc.deudasapp.viewcontrollers.fragments.DeudasFragment;
+import pe.edu.upc.deudasapp.viewcontrollers.fragments.SettingFragment;
 
 import static android.content.res.Configuration.ORIENTATION_PORTRAIT;
 
 public class DeudaActivity extends AppCompatActivity {
- 
+
     private static String TAG = "DeudasApp";
     private TextView departamentoTextView;
     private TextView periodoTextView;
     private TextView montoTextView;
-    private TextView perfilTextView;
+    private TextView estadoTextView;
+    private EditText operacionTextEdit;
+    private TextView mensajeTextView;
+    private LinearLayout linearLayout;
+
+    private Button buttonPagar;
+    private Button buttonConfirmar;
+    private Button buttonListado;
 
     private RecyclerView deudaDetallesRecyclerView;
     private DeudaDetalleAdapter deudaDetallesAdapter;
     private RecyclerView.LayoutManager deudaDetallesLayoutManager;
     private List<DeudaDetalle> detalles;
+
+    private int idDeuda;
 
 
     @Override
@@ -63,49 +78,87 @@ public class DeudaActivity extends AppCompatActivity {
         departamentoTextView = (TextView) findViewById(R.id.text_departamento);
         periodoTextView = (TextView) findViewById(R.id.text_periodo);
         montoTextView = (TextView) findViewById(R.id.text_monto);
-        perfilTextView = (TextView) findViewById(R.id.text_perfil);
+        estadoTextView = (TextView) findViewById(R.id.text_estado);
+        operacionTextEdit = (EditText) findViewById(R.id.text_operacion);
+        mensajeTextView = (TextView) findViewById(R.id.text_mensaje_label);
+        linearLayout = (LinearLayout) findViewById(R.id.linearLayout);
 
-        int idDeuda = Deuda.from(intent.getExtras());
-        setDeuda(idDeuda, context);
+        buttonPagar = (Button) findViewById(R.id.button_pagar);
+        buttonConfirmar = (Button) findViewById(R.id.button_confirmar);
+        buttonListado = (Button) findViewById(R.id.button_listado);
+
+        idDeuda = Deuda.from(intent.getExtras());
+        setDeuda(context);
     }
 
-    private void setDeuda(int idDeuda, final Context context) {
-        String numTel = "123";
+    private void setDeuda(final Context context) {
+        String numTel = Usuario.getNumeroTelefono();
         AndroidNetworking.get(NewsApi.getDeudaUrl())
-            .addQueryParameter("tel", numTel)
-            .addQueryParameter("id", ""+idDeuda)
-            .setTag(TAG)
-            .setPriority(Priority.LOW)
-            .build()
-            .getAsJSONObject(new JSONObjectRequestListener() {
-                @Override
-                public void onResponse(JSONObject response) {
-                    Log.d(TAG, String.format("Response : ",response));
-                    try {
-                        if("error".equalsIgnoreCase(response.getString("status"))) {
-                            Log.d(TAG, String.format("Response Error: %s",
-                                    response.getString("message")));
-                            return;
+                .addQueryParameter("tel", numTel)
+                .addQueryParameter("id", ""+idDeuda)
+                .setTag(TAG)
+                .setPriority(Priority.LOW)
+                .build()
+                .getAsJSONObject(new JSONObjectRequestListener() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d(TAG, String.format("Response : ",response));
+                        try {
+                            if("error".equalsIgnoreCase(response.getString("status"))) {
+                                Log.d(TAG, String.format("Response Error: %s",
+                                        response.getString("message")));
+                                return;
+                            }
+                            List<Deuda> deudas = Deuda.from(response.getJSONArray("deudas"));
+                            updateViews(deudas.get(0), context);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
                         }
-                        List<Deuda> deudas = Deuda.from(response.getJSONArray("deudas"));
-                        updateViews(deudas.get(0), context);
-                    } catch (JSONException e) {
-                        e.printStackTrace();
                     }
-                }
 
-                @Override
-                public void onError(ANError anError) {
-                    Log.d(TAG, String.format("anError: %s", anError.getErrorDetail()));
-                }
-            });
+                    @Override
+                    public void onError(ANError anError) {
+                        Log.d(TAG, String.format("anError: %s", anError.getErrorDetail()));
+                    }
+                });
     }
 
     private void updateViews(Deuda deuda, Context context ) {
         departamentoTextView.setText(deuda.getDepartamento());
         periodoTextView.setText(deuda.getPeriodo());
         montoTextView.setText(String.valueOf(deuda.getMonto()));
-        perfilTextView.setText(deuda.getPeriodo());
+        estadoTextView.setText(deuda.getEstado());
+        operacionTextEdit.setText(deuda.getOperacion());
+
+        if(deuda.getEstado().equals(Constantes.ESTADO_PENDIENTE)){
+            linearLayout.setBackgroundColor(Color.RED);
+            buttonConfirmar.setVisibility(View.INVISIBLE);
+            if(deuda.getPerfil() == Constantes.INQUILINO){
+                buttonPagar.setVisibility(View.VISIBLE);
+                operacionTextEdit.setEnabled(true);
+            }else{
+                buttonPagar.setVisibility(View.INVISIBLE);
+                operacionTextEdit.setEnabled(false);
+            }
+        }else if(deuda.getEstado().equals(Constantes.ESTADO_EN_PROCESO)){
+            linearLayout.setBackgroundColor(Color.YELLOW);
+            buttonPagar.setVisibility(View.INVISIBLE);
+            operacionTextEdit.setEnabled(false);
+            if(deuda.getPerfil() == Constantes.ADMINISTRADOR){
+                buttonConfirmar.setVisibility(View.VISIBLE);
+            }else{
+                buttonConfirmar.setVisibility(View.INVISIBLE);
+            }
+        } else {//Pagado
+            linearLayout.setBackgroundColor(Color.GREEN);
+            buttonPagar.setVisibility(View.INVISIBLE);
+            buttonConfirmar.setVisibility(View.INVISIBLE);
+            operacionTextEdit.setEnabled(false);
+        }
+        buttonPagar.setOnClickListener(onClickPagarListener);
+        buttonConfirmar.setOnClickListener(onClickConfirmarListener);
+        buttonListado.setOnClickListener(onClickListadoListener);
+
 
         deudaDetallesRecyclerView = (RecyclerView) findViewById(R.id.recycler_detalles);
         detalles = new ArrayList<>();
@@ -118,6 +171,111 @@ public class DeudaActivity extends AppCompatActivity {
         deudaDetallesAdapter.notifyDataSetChanged();
 
     }
+
+    private View.OnClickListener onClickPagarListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if(operacionTextEdit.getText().toString().trim().equals("")){
+                mensajeTextView.setText("Ingrese numero de operación");
+            }else{
+                String numTel = Usuario.getNumeroTelefono();
+                AndroidNetworking.get(NewsApi.getPagarUrl())
+                        .addQueryParameter("id", "" + idDeuda)
+                        .addQueryParameter("ope", operacionTextEdit.getText().toString())
+                        .addQueryParameter("est", "En proceso")
+                        .setTag(TAG)
+                        .setPriority(Priority.LOW)
+                        .build()
+                        .getAsJSONObject(new JSONObjectRequestListener() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                Log.d(TAG, String.format("Response : ", response));
+                                try {
+                                    if ("error".equalsIgnoreCase(response.getString("status"))) {
+                                        mensajeTextView.setText("ERROR REALIZANDO LA OPERACIÓN");
+                                    } else {
+                                        Deuda deuda = new Deuda(idDeuda);
+                                        Context context = DeudaActivity.this.getApplicationContext();
+                                        context.startActivity(new Intent(context,
+                                                DeudaActivity.class).putExtras(deuda.toBundle()));
+                                    }
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+
+                            @Override
+                            public void onError(ANError anError) {
+                                Log.d(TAG, String.format("anError: %s", anError.getErrorDetail()));
+                            }
+                        });
+            }
+        }
+    };
+
+    private View.OnClickListener onClickConfirmarListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            String numTel = Usuario.getNumeroTelefono();
+            AndroidNetworking.get(NewsApi.getConfirmarUrl())
+                    .addQueryParameter("id", ""+idDeuda)
+                    .addQueryParameter("est", "Pagado")
+                    .setTag(TAG)
+                    .setPriority(Priority.LOW)
+                    .build()
+                    .getAsJSONObject(new JSONObjectRequestListener() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            Log.d(TAG, String.format("Response : ",response));
+                            try {
+                                if("error".equalsIgnoreCase(response.getString("status"))) {
+                                    mensajeTextView.setText("ERROR REALIZANDO LA OPERACIÓN");
+                                }else{
+                                    Deuda deuda = new Deuda(idDeuda);
+                                    Context context = DeudaActivity.this.getApplicationContext();
+                                    context.startActivity(new Intent(context,
+                                            DeudaActivity.class).putExtras(deuda.toBundle()));
+                                }
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+
+                        @Override
+                        public void onError(ANError anError) {
+                            Log.d(TAG, String.format("anError: %s", anError.getErrorDetail()));
+                        }
+                    });
+
+        }
+    };
+
+    private View.OnClickListener onClickListadoListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            /*Context context = DeudaActivity.this.getApplicationContext();
+            Intent intent = new Intent(DeudaActivity.this, DeudasFragment.class);
+            startActivity(intent);*/
+
+            /*FragmentManager manager = getFragmentManager();
+            FragmentTransaction transaction = manager.beginTransaction();
+            transaction.replace(R.id.content,DeudasFragment(), DeudasFragment.class.getName());
+            transaction.addToBackStack(null);
+            transaction.commit();*/
+            /*getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(android.R.id.content, new DeudasFragment())
+                    .addToBackStack(DeudaActivity.class.getName())
+                    .commit();*/
+
+            /*DeudasFragment fragment = new DeudasFragment();
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            transaction.replace(android.R.id.content, fragment);
+            transaction.commit();*/
+
+        }
+    };
 
     private int getSpanCount(Configuration configuration) {
         return configuration.orientation == ORIENTATION_PORTRAIT ? 1 : 1;
